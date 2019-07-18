a2e15cb44f90d826b524007337e6ab9a8d269c4d
Jeremy Bishow <jeremybishow@gmail.com>
2019-07-17 18:13:05 -0700

    [feat] express serves up static html

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('Express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+const port = 4000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, '../dist/')));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
